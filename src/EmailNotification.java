a54cf39e268e06d5687e29bfc9a283ec41d75a8e
Varun Tiwari <varuntiwari@Varuns-MacBook-Pro.local>
2026-01-30 22:58:16 +0530

    Update notification implementations with API migration comments

--- src/EmailNotification.java
+++ src/EmailNotification.java
@@ -2,5 +2,8 @@ public class EmailNotification implements Notification{
     @Override
     public void sendNotification(String message) {
         System.out.println(message + " Sent by email");
+
+        // Old: Twilio API
+        // New: AWS SNS API
     }
 }
--- src/SMSNotification.java
+++ src/SMSNotification.java
@@ -2,5 +2,8 @@ public class SMSNotification implements Notification {
     @Override
     public void sendNotification(String message) {
         System.out.println(message + " Sent by SMS");
+
+        // Old: Twilio API
+        // New: AWS SNS API
     }
 }
